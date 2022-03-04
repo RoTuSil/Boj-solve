e74e2dda81a30028a420f0d5217600523ee33da2
L0XYG3N <64220019+L0XYG3N@users.noreply.github.com>
2022-03-04 14:47:20 +0900

    [Bronze I] Title: 진법 변환 2, Time: 0 ms, Memory: 1116 KB -BaekjoonHub

---
+++ 백준/Bronze/11005. 진법 변환 2/진법 변환 2.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+void makeN(n,b) {
+	if(n==0)return;
+	makeN(n/b,b);
+	if(n%b<10) printf("%d",n%b);
+	else printf("%c",n%b-10+'A');
+}
+
+int main() {
+	int n,b;
+	char ans[10]={0};
+	scanf("%d %d",&n,&b);
+	makeN(n,b);
+	
+}
