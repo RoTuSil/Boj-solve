3ce254b99af81c05b226228fad67231262e7146b
L0XYG3N <64220019+L0XYG3N@users.noreply.github.com>
2022-03-18 17:09:56 +0900

    [Silver III] Title: N과 M (4), Time: 4 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/15652. N과 M （4）/N과 M （4）.cc
@@ -0,0 +1,26 @@
+#include <iostream>
+using namespace std;
+
+int n, m;
+bool visited[9];
+int arr[9];
+
+void visit(int count, int startNum) {
+	if (m == count) {
+		for (int i = 0; i < m; i++) printf("%d ",arr[i]);
+		printf("\n");
+		return;
+	}
+	for (int i = startNum; i <= n; i++) {
+		//if (visited[i]) continue;
+		visited[i] = true;
+		arr[count] = i;
+		visit(count + 1, i);
+		visited[i] = false;
+	}
+}
+
+int main() {
+	cin >> n >> m;
+	visit(0, 1);
+}
