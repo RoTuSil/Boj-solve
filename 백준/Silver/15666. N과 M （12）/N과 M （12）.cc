f9764a6a36c1d66ffc9d4f009566a25fa02a97c8
L0XYG3N <64220019+L0XYG3N@users.noreply.github.com>
2022-03-23 14:27:53 +0900

    [Silver II] Title: N과 M (12), Time: 4 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/15666. N과 M （12）/N과 M （12）.cc
@@ -0,0 +1,40 @@
+#include <iostream>
+using namespace std;
+
+int arr[8] = {};
+int arrPrint[8] = {};
+int n, m;
+bool visited[8] = {};
+
+void NofM(int depth, int idx) {
+	if (depth == m) {
+		for (int i = 0; i < m; i++)
+			printf("%d ", arrPrint[i]);
+		printf("\n");
+		return;
+	}
+	int lastindex = -1;
+	for (int i = idx; i < n; i++) {
+		if (lastindex == arr[i]) continue;
+
+		visited[i] = true;
+		arrPrint[depth] = arr[i];
+		NofM(depth + 1, i); 
+		visited[i] = false;
+		lastindex = arr[i];
+	}
+}
+
+int main() {
+	cin >> n >> m;
+	for (int i = 0; i < n; i++) {
+		cin >> arr[i];
+	}
+	for (int i = 0; i < n - 1; i++) {
+		for (int j = 0; j < n - 1; j++) {
+			if (arr[j] > arr[j + 1]) 
+				arr[j] ^= arr[j + 1] ^= arr[j] ^= arr[j + 1];
+		}
+	}
+	NofM(0,0);
+}
