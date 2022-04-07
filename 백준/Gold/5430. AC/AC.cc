04cb01e6c2e7c576204495b22827e8233f36bb65
L0XYG3N <64220019+L0XYG3N@users.noreply.github.com>
2022-04-07 18:52:10 +0900

    [Gold V] Title: AC, Time: 108 ms, Memory: 3380 KB -BaekjoonHub

---
+++ 백준/Gold/5430. AC/AC.cc
@@ -0,0 +1,61 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	int tc;
+	cin >> tc;
+	for (int t = 0; t < tc; t++) {
+		string command;
+		string s;
+		int len;
+		int arr[100000] = {};
+		bool reversed = false;
+		cin >> command;
+		cin >> len;
+		cin >> s;
+		for (int i = 0, num = 0, digit = 1, index = 0; i < s.length(); i++) {
+			if (s[i] >= '0' && s[i] <= '9') {
+				num *= 10;
+				num += s[i] - '0';
+				continue;
+			}
+			if (s[i] == ']' || s[i] == ',') {
+				arr[index++] = num;
+				num = 0;
+				digit = 1;
+			}
+		}
+		int front = 0, back = len;
+		bool error = false;
+		for (int i = 0; i < command.length(); i++) {
+			if (command[i] == 'R') {
+				reversed = (reversed ? false : true);
+			} else {
+				reversed ? back-- : front++;
+				if (front > back) {
+					error = true;
+					cout << "error\n";
+					break;
+				}
+			}
+		}
+		if (error) continue;
+		cout << "[";
+		if (reversed) {
+			for (int i = back - 1; i >= front; i--) {
+				cout << arr[i];
+				if (i != front) {
+					cout << ",";
+				}
+			}
+		} else {
+			for (int i = front; i < back; i++) {
+				cout << arr[i];
+				if (i != back - 1) {
+					cout << ",";
+				}
+			}
+		}
+		cout << "]\n";
+	}
+}
