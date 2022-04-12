0553e4cef15ccbf5bb60fd8813df3250876194ea
L0XYG3N <64220019+L0XYG3N@users.noreply.github.com>
2022-04-12 14:58:26 +0900

    [Silver IV] Title: 듣보잡, Time: 108 ms, Memory: 7448 KB -BaekjoonHub

---
+++ 백준/Silver/1764. 듣보잡/듣보잡.cc
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <map>
+#include <vector>
+#include <algorithm>
+using namespace std;
+int main() {
+	map<string, int> nosee;
+	vector<string> noHearNoSee;
+	string name;
+	int n, m;
+	cin >> n >> m;
+	for (int i = 0; i < n; i++) {
+		cin >> name;
+		nosee.insert({ name, i });
+	}
+	for (int j = 0; j < m; j++) {
+		cin >> name;
+		if (nosee.find(name) != nosee.end()) {
+			noHearNoSee.push_back(name);
+		}
+	}
+	sort(noHearNoSee.begin(),noHearNoSee.end());
+	cout << noHearNoSee.size() << "\n";
+	for (int i = 0; i < noHearNoSee.size(); i++) {
+		cout << noHearNoSee[i] << "\n";
+	}
+}
