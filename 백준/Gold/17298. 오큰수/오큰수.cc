42cce745a68c5e99f2b2f178a9c803b316ee2c56
L0XYG3N <64220019+L0XYG3N@users.noreply.github.com>
2022-04-22 19:29:28 +0900

    [Gold IV] Title: 오큰수, Time: 256 ms, Memory: 12816 KB -BaekjoonHub

---
+++ 백준/Gold/17298. 오큰수/오큰수.cc
@@ -0,0 +1,35 @@
+#include <iostream>
+#include <stack>
+using namespace std;
+
+stack<int> nge;
+stack<int> answer;
+int arr[1000001];
+int main() {
+	ios::sync_with_stdio(0);
+	cin.tie(0);
+	cout.tie(0);
+	int n;
+	cin >> n;
+	for (int i = 0; i < n; i++) {
+		cin >> arr[i];
+	}
+	for (int i = n - 1; i >= 0; i--) {
+		while (1) {
+			if (nge.empty()) {
+				answer.push(-1);
+				break;
+			}
+			if (nge.top() > arr[i]) {
+				answer.push(nge.top());
+				break;
+			}
+			if (nge.top() <= arr[i]) nge.pop();
+		}
+		nge.push(arr[i]);
+	}
+	while (!answer.empty()) {
+		cout << answer.top() << " ";
+		answer.pop();
+	}
+}
