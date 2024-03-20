e426e287a24d0d7a3b65255bfa7db9b4e6410f35
beom_k <64220019+L0XYG3N@users.noreply.github.com>
2024-03-20 17:21:24 +0900

    [Silver III] Title: 자전거 묘기, Time: 100 ms, Memory: 6580 KB -BaekjoonHub

---
+++ 백준/Silver/25706. 자전거 묘기/자전거 묘기.cc
@@ -0,0 +1,27 @@
+#include <iostream>
+using namespace std;
+
+int ans[200001];
+int ramp[200001];
+int n;
+
+int g(int p) {
+	if (p >= n) return 0;
+	if (ans[p] != 0) return ans[p];
+	if (ramp[p] == 0) {
+		ans[p] = g(p + 1) + 1;
+	}
+	else ans[p] = g(p + ramp[p]+1) + 1;
+
+	return ans[p];
+	
+}
+
+
+int main() {
+	cin >> n;
+	for (int i = 0; i < n; i++)
+		cin >> ramp[i];
+	for (int i = 0; i < n; i++)
+		cout << g(i) << " ";
+}
